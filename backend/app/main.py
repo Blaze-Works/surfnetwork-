f43d8738246f0637e910eb00bfdde6342d70ce1f
Blaze-Works <55857279+Blaze-Works@users.noreply.github.com>
2025-09-24 08:19:03 +0100

    Work!

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="SurfNetwork API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[os.getenv("DEV_IP"), "https://surfnetwork.xyz"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
